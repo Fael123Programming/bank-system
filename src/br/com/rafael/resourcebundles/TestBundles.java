254ef16d5a181265fe1607bc7cccb5e7bf652114
Fael123Programming <rafaelfonseca1020@gmail.com>
2021-12-24 20:22:56 -0300

    Implementing resource bundles and language-specific resources (internationalization)

---
+++ src/br/com/rafael/resourcebundles/TestBundles.java
@@ -0,0 +1,12 @@
+package br.com.rafael.resourcebundles;
+
+import java.util.ResourceBundle;
+import java.util.Locale;
+
+public class TestBundles {
+    public static void main(String[] args) {
+        ResourceBundle ptBundle = ResourceBundle.getBundle("br.com.rafael.resourcebundles.messages",
+                new Locale("fr", "FR"));
+        System.out.println(ptBundle.getString("menuOptions"));
+    }
+}
